64decac6f27cfb2f8b8397337c3b7b96f5471c67
brian peever <brian.peever@gmail.com>
2023-12-16 12:05:02 -0600

    Added CSRF_TRUSTED_ORIGINS to settings.prod

--- sidequestapi/settings/prod.py
+++ sidequestapi/settings/prod.py
@@ -14,6 +14,8 @@ CHANNEL_LAYER_REDIS_URL = os.environ['CHANNEL_LAYER_REDIS_URL']
 MIDDLEWARE.insert(1,'whitenoise.middleware.WhiteNoiseMiddleware')
 
 ALLOWED_HOSTS = ['*', "sidequestapi-v2-3ac308dc66da.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["*", "http://sidequestapi-v2-3ac308dc66da.herokuapp.com"]
+
 
 django_heroku.settings(locals())
 
